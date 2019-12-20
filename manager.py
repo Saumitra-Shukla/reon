3ed19dc91e700441634fa3ab62e2128b321fab3a
Leon <leon.hillmann@googlemail.com>
2019-12-20 14:19:11 +0100

    basic manager functionality

--- manager.py
+++ manager.py
@@ -1,5 +1,14 @@
 #! /usr/env/python3 
-
-import termcolor
 import os 
+# Processes: [
+#  ["Process name", "path_to_start"]
+# ]
+processes = [
+    ["model","./model/main.py"]
+]
 
+
+
+for i in processes:
+    print("Starting process {}".format(i[0]))
+    os.system("python3 "+str(i[1]))
--- model/main.py
+++ model/main.py
@@ -1,24 +1,24 @@
 #! /usr/env/python3
-import matplotlib.image as mpimg
 from yolo_pipeline import *
 from lane import *
 import numpy as np
 import cv2
+import os
 
 def pipeline_yolo(img):
     img_undist, img_lane_augmented, lane_info = lane_process(img)
     output = vehicle_detection_yolo(img_undist, img_lane_augmented, lane_info)
     return output
 
-cap = cv2.VideoCapture('example_data/project_video.mp4')
-print(cap)
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    yolo_result = pipeline_yolo(frame)
-    cv2.imshow('frame',yolo_result)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def start_model():
+    cap = cv2.VideoCapture('example_data/project_video.mp4')
+    print(cap)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        yolo_result = pipeline_yolo(frame)
+        cv2.imshow('frame',yolo_result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
-
+    cap.release()
+    cv2.destroyAllWindows()
