35a34b76902625273573d2343bc4494cb9107481
Leon Hillmann <leonhillmann@Leons-MBP.fritz.box>
2019-12-20 17:32:26 +0100

    readme in manager.py and todo

--- manager.py
+++ manager.py
@@ -10,3 +10,6 @@ processes = [
 for i in processes:
     print("started process {}".format(i[0]))
     os.system(i[1])
+
+# This above is starting the processes after each other 
+# TODO: Engage processes when car is driving and not just standing
